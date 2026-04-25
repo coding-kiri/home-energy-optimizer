8ef1bdb15528f2b60e92a0f16e95ca853eac565a
piotr-gayczak <piotrek.gayczak@gmail.com>
2026-04-25 17:11:24 +0200

    Remove: unecessary AWS resources

--- infra/terraform/secrets.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_secretsmanager_secret" "entsoe_token" {
-  name        = "entsoe/token"
-  description = "ENTSO-E Transparency Platform API token. Set the value manually after apply."
-}
